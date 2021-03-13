c150cf9ce31cb7df6a3819ffe20a18335c28127f
umarwanier@gmail.com <you@example.com>
2021-03-13 15:32:13 +0530

    edit

--- Early_Stage_Diabetes/settings.py
+++ Early_Stage_Diabetes/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k3r3uzz42@lo=u$z_3u2&x3cx13#^p+z$xama2vpck#%!z&i$='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['diabetes2-predictor.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
